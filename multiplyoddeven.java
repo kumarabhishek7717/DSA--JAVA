c946400ea4f890b6ed957f44721741ea22076600
kumarabhishek7717 <abhishekkumar7717749@gmail.com>
2025-10-13 15:28:21 +0530

    Update multiplyoddeven.java
    ADDED ME

--- multiplyoddeven.java
+++ multiplyoddeven.java
@@ -1,15 +1,20 @@
 public class multiplyoddeven {
-    public static void main(String[] args) {
-        int [] arr = {57,98,90,45,67,34,23,76};
-        for (int i = 0; i<arr.length; i++){
-            if (arr[i] %2==1) {
-            arr[i] *=2;
-
-            }
-            else arr[i] +=10;{
-            }
-            System.out.println(arr[i]);
+   public static void main(String[] args) {
+    int [] arr = {3,7,4,5,3,7,9};
+    print (arr);
+    for (int i = 0; i<arr.length;i++){
+        if (i%2==1) arr[i] *=2;
+         else 
+            arr[i] +=10;
         }
-    }
+         print (arr);
+        }
     
+    public static void print (int [] arr){
+        for (int i = 0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println( );
+    }
 }
+   
