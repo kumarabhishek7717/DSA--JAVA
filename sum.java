d9f1e0a1550b09cb78a292f78f3d7689db903295
kumarabhishek7717 <abhishekkumar7717749@gmail.com>
2025-10-13 14:02:27 +0530

    Create sum.java
    ADDED ME

---
+++ sum.java
@@ -0,0 +1,12 @@
+public class sum {
+    public static void main(String[] args) {
+        int [] arr = {23,87,465,4567,890,6343};
+        int sum = 0;
+        for (int i = 0; i<arr.length; i++){
+            sum += arr[i];
+        }
+        System.out.println(sum);
+    }
+    
+    
+}
