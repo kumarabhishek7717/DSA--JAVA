7498affe6cef1e85b45a27f67df0f0d36f38c24e
kumarabhishek7717 <abhishekkumar7717749@gmail.com>
2025-10-29 17:51:40 +0530

    Create day.java
    ADDED ME

---
+++ day.java
@@ -0,0 +1,33 @@
+ import java.util.*;
+
+
+public class day {
+
+    
+
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int weaknumber=sc.nextInt();
+        switch(weaknumber){
+            case 1:System.out.println("sunday");
+            break;
+            case 2:System.out.println("monday");
+            break;
+            case 3:System.out.println("tuesday");
+            break;
+            case 4:System.out.println("wednesday");
+            break;
+            case 5:System.out.println("thursday");
+            break;
+            case 6:System.out.println("friday");
+            break;
+            case 7:System.out.println("saturday");
+            break;
+            default :System.out.println("no any weak number");
+            
+        }
+    }
+}
+
+    
+
