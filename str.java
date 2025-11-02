f3663fe3a2170216d34f08e803978fd1d39db959
kumarabhishek7717 <abhishekkumar7717749@gmail.com>
2025-11-02 20:59:53 +0530

    Create str.java
    README

---
+++ str.java
@@ -0,0 +1,66 @@
+public class str {
+    public static void main(String[] args){
+        String str = "abhishek kumar";
+        int length = str.length();
+        System.out.println(length);
+    }
+    
+}
+
+
+// Write a java program to input two strings and print which one is longer ??
+
+
+public class str {
+
+     public static void main(String[] args) {
+        String str = "java";
+        String str1 = "programming";
+        if (str.length() > str1.length()){
+            System.out.println(str);
+        } else {
+            System.out.println(str1);
+        }
+     }
+}
+
+// Count total characters (excluding spaces)
+// write a java program to count the total number of characters in a string excluding spaces??
+
+
+public class str {
+    public static void main (String [] args){
+        String str = "hello world";
+        System.out.println(str.length());
+    }
+}
+
+public class str {
+
+    public static void main(String[] args) {
+        String str = "I Love Java";
+        int average = str.length()/3;
+        System.out.println(average);
+    }
+}  
+
+
+public class str {
+      public static void main(String[] args) {
+        String str = "hello world java programming Language";
+        String str1 = "my name is sarthak sinha";
+        if (str.length() > str1.length()){
+            System.out.println(str);
+        }
+        else {
+            System.out.println(str1);
+        }
+    }
+
+   
+        
+      
+     }
+
+
+
